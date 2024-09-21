5580cefa07c9cc44c15483a98dccff130b3abff1
MAIN-PC\masay <maaboo0812@gmail.com>
2024-09-21 18:45:03 +0900

    CSSファイルの変更

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -2,4 +2,4 @@ const now = new Date();
 const year = now.getFullYear();
 const month = now.getMonth() + 1;
 const date = now.getDate();
-console.log(year, "年", month, "月", date, "日");
+console.log(year + "年" + month + "月" + date + "日");
